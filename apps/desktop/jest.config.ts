357e71f7309320192fa9067a808e8079cf59b3f8
HaHT <haht@creativeforce.io>
2024-10-04 17:46:03 +0700

    refactor app name

--- apps/desktop/jest.config.ts
+++ apps/desktop/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'cf-electron',
+  displayName: 'desktop',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/cf-electron',
+  coverageDirectory: '../../coverage/apps/desktop',
 };
--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'lh-app',
+  displayName: 'web',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/lh-app',
+  coverageDirectory: '../../coverage/apps/web',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
